7066239072367e96959c95e9f4d9cbd661f63fa8
Eemeli Aro <eemeli@gmail.com>
2019-02-22 20:35:48 +0200

    Always test polyfill

--- test/polyfill.js
+++ test/polyfill.js
@@ -1,21 +1,21 @@
 var expect = require('expect.js');
-require('../polyfill');
+var PluralRules = require('../plural-rules').default;
 
 describe('Intl.PluralRules polyfill', function(){
     it('should exist', function(){
-        expect(Intl.PluralRules).to.be.a('function');
+        expect(PluralRules).to.be.a('function');
     });
     describe('.supportedLocalesOf()', function(){
         it('should be executable', function(){
-            expect(Intl.PluralRules.supportedLocalesOf).to.not.throwException(console.log);
+            expect(PluralRules.supportedLocalesOf).to.not.throwException(console.log);
         });
         it('should return an empty array when called with no args', function(){
-            var res = Intl.PluralRules.supportedLocalesOf();
+            var res = PluralRules.supportedLocalesOf();
             expect(res).to.be.an('array');
             expect(res).to.be.empty();
         });
         it('should return a valid array', function(){
-            var res = Intl.PluralRules.supportedLocalesOf(['en', 'fi-FI']);
+            var res = PluralRules.supportedLocalesOf(['en', 'fi-FI']);
             expect(res).to.be.an('array');
             expect(res).to.have.length(2);
             expect(res[0]).to.equal('en');
@@ -25,14 +25,14 @@ describe('Intl.PluralRules polyfill', function(){
 
     describe('constructor', function(){
         it('should require `new`', function(){
-            expect(Intl.PluralRules).to.throwException(function(e){
+            expect(PluralRules).to.throwException(function(e){
                 expect(e).to.be.a(TypeError);
             });
-            var fn = function(){ new Intl.PluralRules(); };
+            var fn = function(){ new PluralRules(); };
             expect(fn).to.not.throwException(console.log);
         });
         it('should select a default type & locale', function(){
-            var p = new Intl.PluralRules();
+            var p = new PluralRules();
             expect(p).to.be.an('object');
             expect(p.select).to.be.a('function');
             var opt = p.resolvedOptions()
@@ -41,7 +41,7 @@ describe('Intl.PluralRules polyfill', function(){
             expect(opt.locale.length).to.be.greaterThan(1);
         });
         it('should handle valid simple arguments correctly', function(){
-            var p = new Intl.PluralRules('fi-FI', { type: 'ordinal' });
+            var p = new PluralRules('fi-FI', { type: 'ordinal' });
             expect(p).to.be.an('object');
             expect(p.select).to.be.a('function');
             var opt = p.resolvedOptions()
@@ -49,7 +49,7 @@ describe('Intl.PluralRules polyfill', function(){
             expect(opt.locale).to.match(/^fi\b/);
         });
         it('should choose a locale correctly from multiple choices', function(){
-            var p = new Intl.PluralRules(['i-klingon', 'ak', 'en']);
+            var p = new PluralRules(['i-klingon', 'ak', 'en']);
             expect(p).to.be.an('object');
             expect(p.select).to.be.a('function');
             var opt = p.resolvedOptions()
@@ -57,7 +57,7 @@ describe('Intl.PluralRules polyfill', function(){
             expect(opt.locale).to.equal('ak');
         });
         it('should complain about invalid types', function(){
-            var fn = function(){ new Intl.PluralRules('en', { type: 'invalid' }); };
+            var fn = function(){ new PluralRules('en', { type: 'invalid' }); };
             expect(fn).to.throwException(function(e){
                 expect(e).to.be.a(RangeError);
             });
@@ -66,11 +66,11 @@ describe('Intl.PluralRules polyfill', function(){
 
     describe('#resolvedOptions()', function(){
         it('should exist', function(){
-            var p = new Intl.PluralRules();
+            var p = new PluralRules();
             expect(p.resolvedOptions).to.be.a('function');
         });
         it('should return expected values', function(){
-            var p = new Intl.PluralRules('fi-FI'), res = p.resolvedOptions();
+            var p = new PluralRules('fi-FI'), res = p.resolvedOptions();
             expect(res).to.be.an('object');
             expect(res.locale).to.match(/^fi\b/);
             expect(res.pluralCategories).to.eql(['one', 'other']);
@@ -80,19 +80,19 @@ describe('Intl.PluralRules polyfill', function(){
 
     describe('#select()', function(){
         it('should return a string', function(){
-            var p = new Intl.PluralRules(), res = p.select();
+            var p = new PluralRules(), res = p.select();
             expect(res).to.be.a('string');
             expect(res).to.equal('other');
         });
         it('should work for English cardinals', function(){
-            var p = new Intl.PluralRules('en', { type: 'cardinal' });
+            var p = new PluralRules('en', { type: 'cardinal' });
             expect(p.select(1)).to.equal('one');
             //expect(p.select('1.0')).to.equal('other');
             expect(p.select(2)).to.equal('other');
             //expect(p.select('-2.0')).to.equal('other');
         });
         it('should work for English ordinals', function(){
-            var p = new Intl.PluralRules('en', { type: 'ordinal' });
+            var p = new PluralRules('en', { type: 'ordinal' });
             expect(p.select(1)).to.equal('one');
             expect(p.select('22')).to.equal('two');
             expect(p.select('3.0')).to.equal('few');
