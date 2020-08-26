701fbfa135797e74620fdad359c345c7c4cfa7bd
Eemeli Aro <eemeli@gmail.com>
2020-08-26 19:44:41 +0300

    Well, clearly the CI example test fails when it should.

--- examples/test.sh
+++ examples/test.sh
@@ -2,5 +2,5 @@
 set -e
 
 node dist/factory.js | grep -q "factory { one: 'one', other: 'other' }" && echo "factory ok" || (echo "factory failed" && false)
-node dist/ponyfill.js | grep -q "ponyfill { one: 'one', othe: 'other' }" && echo "ponyfill ok" || (echo "ponyfill failed" && false)
+node dist/ponyfill.js | grep -q "ponyfill { one: 'one', other: 'other' }" && echo "ponyfill ok" || (echo "ponyfill failed" && false)
 node dist/polyfill.js | grep -q "{ one: 'one', other: 'other' }" && echo "polyfill ok" || (echo "polyfill failed" && false)
