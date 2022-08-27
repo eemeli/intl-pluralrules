0d37a7a507536d7d3319d587f2c6cc14b7e355ef
Eemeli Aro <eemeli@gmail.com>
2022-08-27 14:04:07 +0300

    chore: Set browserslist targets in package.json

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,5 @@ const modules =
   process.env.NODE_ENV === 'test' ? 'auto' : process.env.TARGET || false
 
 module.exports = {
-  presets: [['@babel/preset-env', { modules, targets: '> 0.25%, not dead' }]]
+  presets: [['@babel/preset-env', { modules }]]
 }
