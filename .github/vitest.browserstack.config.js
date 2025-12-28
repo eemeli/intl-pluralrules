acfc6e586580d2a288e7cb4a3be043ee978b1b4c
Eemeli Aro <eemeli@gmail.com>
2025-12-28 19:08:54 +0200

    test: Switch from Mocha & Chai to Vitest

---
+++ .github/vitest.browserstack.config.js
@@ -0,0 +1,61 @@
+import { env } from 'node:process'
+import { defineConfig } from 'vitest/config'
+import { playwright } from '@vitest/browser-playwright'
+
+const bsDefaults = {
+  project: env.BROWSERSTACK_PROJECT_NAME,
+  build: process.env.BROWSERSTACK_BUILD_NAME,
+  'browserstack.idleTimeout': 600,
+  'browserstack.local': 'true',
+  'browserstack.localIdentifier': env.BROWSERSTACK_LOCAL_IDENTIFIER,
+  'browserstack.username': env.BROWSERSTACK_USERNAME,
+  'browserstack.accessKey': env.BROWSERSTACK_ACCESS_KEY || env.BROWSERSTACK_KEY
+}
+
+const bsProvider = opts =>
+  playwright({
+    connectOptions: {
+      wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(
+        JSON.stringify({ ...bsDefaults, ...opts })
+      )}`
+    }
+  })
+
+export default defineConfig({
+  test: {
+    // api: { host: '0.0.0.0' },
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      connectTimeout: 300_000,
+      instances: [
+        {
+          name: 'Chrome Old',
+          browser: 'chromium',
+          provider: bsProvider({ browser: 'chrome', browser_version: '110' })
+        },
+        {
+          name: 'Chrome New',
+          browser: 'chromium',
+          provider: bsProvider({ browser: 'chrome', browser_version: 'latest' })
+        },
+        {
+          name: 'Firefox Old',
+          browser: 'chromium',
+          provider: bsProvider({
+            browser: 'playwright-firefox',
+            browser_version: '110'
+          })
+        },
+        {
+          name: 'Firefox New',
+          browser: 'chromium',
+          provider: bsProvider({
+            browser: 'playwright-firefox',
+            browser_version: 'latest'
+          })
+        }
+      ]
+    }
+  }
+})
--- test/native-numberformat.test.mjs
+++
@@ -1,5 +0,0 @@
-import PluralRules from '../src/plural-rules.mjs'
-
-import { suite } from './test-suite.mjs'
-
-describe('With native Intl.NumberFormat', () => suite(PluralRules))
